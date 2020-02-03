960cb44ff9db7c4d44ae17a59ea55c9c58ac117f
paul sorenson <pms@deadpool.home.metrak.com>
2020-02-03 22:11:42 +1100

    [sentry] --dry-run added

--- src/sentry_stack.py
+++ src/sentry_stack.py
@@ -8,7 +8,7 @@ from collections import defaultdict
 from dataclasses import dataclass
 from datetime import datetime
 from pathlib import Path
-from typing import Generator, List
+from typing import Dict, Generator, List, Tuple
 
 log = logging.getLogger()
 logging.basicConfig()
@@ -74,8 +74,15 @@ class SentryEvent:
         )
 
 
-def get_args() -> Namespace:
-    ap = ArgumentParser()
+def get_args() -> Tuple[Namespace, List]:
+    ap = ArgumentParser(
+        description="Stack your Tesla model3 sentry videos into a single output. "
+        "WARNING: any arguments not explicitly recognized are considered "
+        "global options which are added to the command line immediately after ffmpeg. "
+        "Consider using the --dry-run argument and also inspect "
+        "the logs for 'global options', if this list is not empty "
+        "then proceed with caution."
+    )
 
     ap.add_argument(
         "--sentry-dir",
@@ -106,12 +113,18 @@ def get_args() -> Namespace:
         help="Silently overwrite output file, otherwise skip.",
     )
     ap.add_argument(
+        "--dry-run",
+        action="store_true",
+        help="print commands but do not execute. "
+        "Note this is for information only, you may need to tweak the escaping.",
+    )
+    ap.add_argument(
         "--list-dir",
         action="store_true",
         help="list sub directories which correspond to Tesla sentry events.",
     )
 
-    opt = ap.parse_args()
+    opt = ap.parse_known_args()
     return opt
 
 
@@ -133,12 +146,33 @@ def event_groups(events_dir: Path,) -> defaultdict:
 
 
 def compose_stack_cmd(
-    event: SentryEvent, scale: int = 4, speed: int = 1, quality: int = 23
+    event: SentryEvent,
+    scale: int = 4,
+    speed: int = 1,
+    quality: int = 23,
+    global_opts: List[str] = None,
 ) -> List[str]:
+    """
+    Compose ffmpeg command to process `SentryEvent` and return it in list
+    form for subprocess run.
+
+    :param event: SentryEvent object.
+    :param scale: divisor for output video dimensions. Note since there
+    are 4 videos in the output, using scale=4 (the default) will result
+    in output size the same as one of the source videos.
+    :param speed: speed up factor.
+    :param quality: increase this to reduce quality and output file size.
+    :param global_opts: any strings provided here are added immediately
+    after the command.
+    """
+
     views = ["front", "back", "left", "right"]
 
     cmd = ["ffmpeg"]
-    # cmd.extend(["-v", "9"])
+
+    if global_opts:
+        cmd.extend(global_opts)
+
     for v in views:
         try:
             cmd.extend(["-i", str(event.event_dir / event.get_view(v))])
@@ -162,8 +196,14 @@ def compose_stack_cmd(
 
 
 def main():
-    opt = get_args()
-    log.info(opt)
+    opt, global_opts = get_args()
+    log.info(f"{opt} global options: {global_opts}")
+
+    if opt.overwrite:
+        # don't need to explicitly set -n for the converse case
+        # because the check is done before the ffmpeg comand is run.
+        global_opts.append("-y")
+
     if opt.list_dir:
         print(list(events_dir_iter(opt.sentry_dir)))
 
@@ -185,18 +225,20 @@ def main():
                 scale=opt.scale, speed=opt.speed, quality=opt.quality
             )
             if op.exists():
-                log.info(
-                    f"output: '{op}' exists, skipping. "
-                    "Add --overwrite on command line to change this."
-                )
+                log.info(f"output: '{op}' exists, skipping. See --overwrite.")
                 continue
         print(event)
         print(event.front)
         cmd = compose_stack_cmd(
-            event, scale=opt.scale, speed=opt.speed, quality=opt.quality
+            event,
+            scale=opt.scale,
+            speed=opt.speed,
+            quality=opt.quality,
+            global_opts=global_opts,
         )
-        print(cmd)
-        sp.run(cmd)
+        print(" ".join(cmd))
+        if not opt.dry_run:
+            sp.run(cmd)
 
 
 if __name__ == "__main__":
